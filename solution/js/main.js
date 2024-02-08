56934deded8f75d1767e7f7cf1c6ba74b7f37148
Jay Yu <jy195@hotmail.com>
2024-02-07 23:39:46 -0500

    add disclaimer

--- solution/js/main.js
+++ solution/js/main.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+*  WEB422 – Assignment 2
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Jihun Yu Student ID: 107890220 Date: Feb-07-2024
+*  Cyclic Link: https://energetic-sundress-deer.cyclic.app/
+*
+********************************************************************************/ 
+
 let page = 1;
 const perPage = 8;
 
