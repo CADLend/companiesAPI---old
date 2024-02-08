19facc5bdfc934b8bef1a3851b96dfc2bcbe677d
Jay Yu <jy195@hotmail.com>
2024-02-07 22:21:28 -0500

    add path

--- server.js
+++ server.js
@@ -11,6 +11,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const cors = require('cors');
 const CompaniesDB = require('./modules/companiesDB.js');
 
